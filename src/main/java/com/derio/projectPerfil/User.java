10b52a05e715ed21b75aa4c0cae3afc4fa57738c
derio123 <deriomatheus6@gmail.com>
2023-03-30 19:10:58 -0300

    alteração no user

--- src/main/java/com/derio/projectPerfil/User.java
+++ src/main/java/com/derio/projectPerfil/User.java
@@ -15,6 +15,9 @@ public class User {
 	
 	private String name;
 	private String email;
+	private String cpf;
+	private String endereco;
+	private String telefone;
 	
 	public String getName() {
 		return name;
@@ -27,6 +30,25 @@ public class User {
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
 	}
 	
 	public Long getId() {
@@ -41,4 +63,6 @@ public class User {
 	public void setImg(byte[] img) {
 		this.img = img;
 	}
+    public void save(User user) {
+    }
 }
