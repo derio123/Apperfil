ec39718167141826264a599fbae22cdd313bd46c
derio123 <deriomatheus6@gmail.com>
2023-07-19 11:05:30 -0300

    baixando appperfil

--- src/main/java/com/derio/projectPerfil/ProjectPerfilApplication.java
+++ src/main/java/com/derio/projectPerfil/ProjectPerfilApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectPerfilApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectPerfilApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPerfilApplication.class, args);
+    }
 }
--- src/main/java/com/derio/projectPerfil/UserController.java
+++ src/main/java/com/derio/projectPerfil/UserController.java
@@ -8,27 +8,27 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-	
-	@Autowired
-	private UserRepository userRepository;
-	
-	@RequestMapping(value = "", method = RequestMethod.GET)
-	public List<User> findAll() {
-		return userRepository.findAll();
-	}
-	
-	@RequestMapping(value = "", method = RequestMethod.POST)
-	public User save(@RequestBody User user) {
-		return userRepository.save(user);
-	}
-	
-	@RequestMapping(value = "", method = RequestMethod.PUT)
-	public User update(@RequestBody User user) {
-		return userRepository.save(user);
-	}
-	
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public void findAll(@PathVariable Long id) {
-		 userRepository.deleteById(id);
-	}
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public User save(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.PUT)
+    public User update(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void findAll(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
 }
